c20116702970c056ab3df47c439b631898c3fdf4
yfractal <yfractal@qq.com>
2013-07-06 10:57:34 +0800

    hw1,lab1

---
+++ homeworks/hw1.py
@@ -0,0 +1,103 @@
+# Name:
+# Login:
+# TA:
+# Section:
+# Q1.
+
+from operator import add, sub
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        op = add
+    else:
+        op = sub
+    return op(a, b)
+
+# Q2.
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest of a, b, c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    "*** YOUR CODE HERE ***"
+    def square_of_two(a,b):
+        return a*a + b*b
+
+    max_ = max(max(a,b),c)
+    min_ = min(min(a,b),c)
+    middle = a + b + c - max_ - min_
+
+    return square_fo_two(max_,min_)
+
+# Q3.
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and false_result otherwise."""
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    "*** YOUR CODE HERE ***"
+
+def t():
+    "*** YOUR CODE HERE ***"
+
+def f():
+    "*** YOUR CODE HERE ***"
+
+# Q4.
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its length.
+
+    >>> a = hailstone(10)  # Seven elements are 10, 5, 16, 8, 4, 2, 1
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+
+    step = 0
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            length = length + 1
+            print(n)
+        else:
+            n = n * 3 + 1
+            length = length + 1
+            print(n)
+    return length + 1
